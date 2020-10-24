a380ffebbdb2b18153fa0abf41735b5eca33e34a
GaganSinghmsit <37043052+GAGANsinghmsitece@users.noreply.github.com>
2020-10-24 15:11:21 +0530

    optimised approach added

--- majority_element_2.cpp
+++ majority_element_2.cpp
@@ -2,19 +2,35 @@
 #include<bits/stdc++.h>
 #include<vector>
 using namespace std;
-/*int majority_element(vector<int>arr){
-	//Moore's Voting algorithm(Optimised approach)
-	int count=0,ele=0;
+vector<int> majority_element(vector<int>arr){
+	//Boyor's Voting algorithm(Optimised approach)
+	int c1=0,c2=0,ele1=-1,ele2=-1,n=arr.size();
+	vector<int> result;
 	for(int i=0;i<arr.size();i++){
-		if(count==0)
-			ele=arr[i];
-		if(arr[i]==ele)
-			count++;
-		else
-			count--;
+		if(arr[i]==ele1)c1++;
+		else if(arr[i]==ele2)c2++;
+		else if(c1==0){
+			ele1=arr[i];
+			c1=1;
+		}
+		else if(c2==0){
+			ele2=arr[i];
+			c2=1;
+		}else{
+			c1--;c2--;
+		}
 	}
-	return ele;
-}*/
+	c1=0;c2=0;
+	for(int i=0;i<arr.size();i++){
+		if(arr[i]==ele1)c1++;
+		if(arr[i]==ele2)c2++;
+	}
+	if(c1>n/3)
+		result.push_back(ele1);
+	if(c2>n/3)
+		result.push_back(ele2);
+	return result;
+}
 vector<int> majority_element(vector<int>arr){
 	//brute force
 	int n=arr.size();
@@ -43,7 +59,7 @@ void PrintVector(vector<int> arr){
 		cout<<arr[i]<<" ";
 }
 int main(){
-	vector<int> arr={1};
+	vector<int> arr={3,2,3};
 	vector<int> result=majority_element(arr);
 	PrintVector(result);
 }
