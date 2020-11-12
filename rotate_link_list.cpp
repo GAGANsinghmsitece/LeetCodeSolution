6d64ab4035556308d3e81e4532485be75a48990c
GaganSinghmsit <37043052+GAGANsinghmsitece@users.noreply.github.com>
2020-11-12 13:18:09 +0530

    Add files via upload

---
+++ rotate_link_list.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+    	int prevdata=head->val;
+    	int temp1=0;
+    	ListNode* temp=head;
+    	while(temp!=NULL){
+    		temp=temp->next;
+    		temp1=temp->val;
+    		temp->val=prevdata;
+    		prevdata=temp1;
+    	}
+    	head->val=prevdata;
+    }
+};
